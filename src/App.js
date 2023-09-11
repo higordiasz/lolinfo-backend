acf852b843cadd316d37fb2f6db0eb2a6afe7d6b
Higor Dias Zuqueto <higordiaszuqueto@gmail.com>
2023-09-11 10:13:47 -0300

    0.0.8
    Heroku deploy

--- src/App.js
+++ src/App.js
@@ -4,35 +4,35 @@ import bodyParser from 'body-parser';
 dotenv.config();
 
 const App = Express();
-
+const APIVERSION = process.env.APIVERSION || 'v1';
 App.use(bodyParser.json());
 App.use(bodyParser.urlencoded({
   extended: true
 }));
 
-
+/*
 App.use((req, res, next) => {
   if ((req.headers["x-forwarded-proto"] || "").endsWith("http"))
     res.redirect(`https://${req.headers.host}${req.url}`);
   else
     next();
 });
-
+*/
 // ROUTERS
 import Champion from './routers/ChampionRouter.js'
-App.use(`/api/${process.env.APIVERSION}/champions/`, Champion)
+App.use(`/api/${APIVERSION}/champions/`, Champion)
 
 import Skin from './routers/SkinRouter.js'
-App.use(`/api/${process.env.APIVERSION}/skins`, Skin)
+App.use(`/api/${APIVERSION}/skins`, Skin)
 
 import Information from './routers/InformationRouter.js'
-App.use(`/api/${process.env.APIVERSION}/info/`, Information)
+App.use(`/api/${APIVERSION}/info/`, Information)
 
 import Items from './routers/ItemsRouter.js'
-App.use(`/api/${process.env.APIVERSION}/items/`, Items)
+App.use(`/api/${APIVERSION}/items/`, Items)
 
 import Build from './routers/BuildRouter.js'
-App.use(`/api/${process.env.APIVERSION}/build/`, Build);
+App.use(`/api/${APIVERSION}/build/`, Build);
 
 export {
   App
